dfba4be9cdb33ade6349778dcb10449cf5ec09f0
Yuvraj Singh Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-07-07 22:05:12 +0530

    Add numerical triangle pattern in numericTriangle.py

---
+++ numericTriangle.py
@@ -0,0 +1,7 @@
+#Triangular Pattern
+#Numerical Triangle
+row = int(input("Enter number of rows: "))
+for row in range (1,6):
+    for col in range (1, row+1):
+        print(col, end=" ")
+    print()
