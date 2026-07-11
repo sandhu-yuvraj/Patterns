e687fc1142371d8cae2c72c68b7ffc5837ac65c2
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-07-11 12:44:34 +0530

    Fix bugs

--- triangularPattern.py
+++ triangularPattern.py
@@ -1,6 +1,6 @@
 #Triangular Pattern
-row = int(input("Enter number of rows: "))
-for row in range (1,row+1):
+n = int(input("Enter number of rows: "))
+for row in range (1,n+1):
     for col in range (row):
         print(row, end=" ")
     print()
