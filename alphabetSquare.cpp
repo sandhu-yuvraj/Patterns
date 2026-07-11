ae2f79dc7598a676b10ab743226cb57444707283
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-07-11 11:44:37 +0530

    Add comment

--- alphabetSquare.cpp
+++ alphabetSquare.cpp
@@ -1,14 +1,16 @@
+// Prints a square of capital letters
+// 65 is the ASCII value of 'A' and it continues till 90 for 'Z'
 #include <iostream>
 using namespace std;
 int main(){
     int n;
     cout<<"Enter a number between 1 and 26 (inclusive): ";
     cin>>n;
-    for (int i =1; i<=n; i++){
-        for (int j =1; j<=n; j++){
-            cout<<(char)(j+64)<<" ";
+    for (int i =1; i<=n; i++){       // Prints rows
+        for (int j =1; j<=n; j++){    // Prints columns
+            cout<<(char)(j+64)<<" ";  // Replace 64 with 96 for small letters 
         }
-        cout<<endl;
+        cout<<endl;   // Changes line after completing each row
     }
     return 0;
 }
