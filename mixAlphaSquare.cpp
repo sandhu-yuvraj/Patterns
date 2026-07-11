651dd8f99be0d426d0769b229837ccbe2d5e66fd
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-07-11 11:58:12 +0530

    Add comment

--- mixAlphaSquare.cpp
+++ mixAlphaSquare.cpp
@@ -1,15 +1,15 @@
-// Prints a square of capital letters
-// 65 is the ASCII value of 'A' and it continues till 90 for 'Z'
-// We print letters column wise
+// Prints a modified alphabet square
+// Prints letters in lower case for odd rows and upper case for even rows
 #include <iostream>
 using namespace std;
-int main(){
+int main (){
     int n;
     cout<<"Enter a number between 1 and 26 (inclusive): ";
     cin>>n;
     for (int i =1; i<=n; i++){       // Prints rows
         for (int j =1; j<=n; j++){    // Prints columns
-            cout<<(char)(j+64)<<" ";  // Replace 64 with 96 for small letters 
+          if (i%2 == 0)  cout<<(char)(i+64)<<" ";  // Prints capital for even rows
+          else cout<<(char)(i+96)<<" "; // Prints small for odd rows
         }
         cout<<endl;   // Changes line after completing each row
     }
