a2dd1eb88b23091120c9aed020ef7927f8dd679b
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-07-12 11:43:38 +0530

    Add floyd's triangle

---
+++ floyd.cpp
@@ -0,0 +1,19 @@
+/*Floyd's triangle is a right-angled triangular array of natural numbers 
+  used in computer science education, named after computer scientist Robert Floyd. 
+  It is built by filling the rows with consecutive numbers starting from 1, 
+  where the first row contains one number, the second has two, and so on.*/
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int a=1 ;
+    for (int i =1; i<=n; i++){
+        for (int j=1; j<=i; j++){
+            cout<<a++<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
