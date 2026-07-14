24c4080693762a8f19c2def22944eab7eeca0b95
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-07-14 11:55:29 +0530

    Add space

--- binaryTriangle.cpp
+++ binaryTriangle.cpp
@@ -7,8 +7,8 @@ int main (){
     cin>>n;
     for (int i =1; i<= n; i++){
         for (int j=1; j<= i; j++){
-            if ((i+j)%2 == 0) cout<<1;
-            else cout<<0;
+            if ((i+j)%2 == 0) cout<<1<<" ";
+            else cout<<0<<" ";
         }
         cout<<endl;
     }
