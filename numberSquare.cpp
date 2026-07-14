a025e4516b19fa3af69e61a887caa40f7b247850
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-07-14 19:58:00 +0530

    Add number square in cpp

---
+++ numberSquare.cpp
@@ -0,0 +1,16 @@
+// Prints a number spiral of input
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Size: ";
+    cin>>n;
+    for (int i=1; i<=n; i++){
+        for (int j=1; j<=n; j++){
+            if (i<=j) cout<<i<<" ";
+            else cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
